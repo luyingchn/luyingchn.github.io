608c4600dae1107f8fbe051619bc2b68c9de202f
YingLew <luyingchn@gmail.com>
2017-06-22 21:20:31 +0800

    iui

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "luyingchn.github.io",
+  "",
   "cdnjs.cloudflare.com"
 ]
 
